0044a96fe719c82dfe38d1ef8ee937906e9359b5
Don McCurdy <don.r.mccurdy@gmail.com>
2021-01-05 22:53:41 -0800

    Remove dist/, fixes #7

--- dist/circular-array.module.js
+++
@@ -1,2 +0,0 @@
-function i(i){this._index=0,this._size=i,this._array=[]}i.prototype._incr=function(){this._index=++this._index%this._size},i.prototype.array=function(){return this._array},i.prototype.push=function(i){this._array[this._index]=i,this._incr()};export{i as CircularArray};
-//# sourceMappingURL=circular-array.module.js.map
